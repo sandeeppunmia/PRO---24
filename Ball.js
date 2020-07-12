174b923df73ac502dc4854916fbc5854e6e12e9e
sandeeppunmia <67103020+sandeeppunmia@users.noreply.github.com>
2020-07-12 03:00:21 -0900

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,25 @@
+class Ball {
+    constructor(x,y) {
+      var options = {
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1,
+      }
+      this.body = Bodies.circle(x,y,30,options);
+  
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      fill("pink");
+      ellipseMode(RADIUS);
+      ellipse(0,0,30,30);
+      pop();
+    }
+  }
+  
